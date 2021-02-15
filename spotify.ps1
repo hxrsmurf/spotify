ccdfca6e1ef80f9b281e445591f6bb9076741e51
Kevin Murphy <kevin@hxrsmurf.info>
2021-02-14 21:22:25 -0600

    Updated original script with better functions

--- spotify.ps1
+++ spotify.ps1
@@ -1,70 +1,211 @@
-function NotUsed{
-	$uri = "https://accounts.spotify.com/api/token"
-	$username = $null
-	$password = $null
-	$pair = "$($username):$($password)"
-	$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
-	$basicAuthValue = "Basic $encodedCreds"
+function initialScope {
+	$client_id = ""
+	$client_secret = ""	
+
+	$accounts_url = "https://accounts.spotify.com/"
+	$authorizeURL = $accounts_url + "authorize?"
+
+	$clientString = "client_id=" + $client_id
+	$responseString = "&response_type=code&redirect_uri="
+	$redirect_uri = "https://kvchmurphy.com/callback/"
+	
+	# https://developer.spotify.com/documentation/general/guides/scopes/
+	$scope = (
+	"user-read-recently-played",
+	"user-top-read",
+	"user-read-playback-position",
+	"user-read-playback-state",
+	"user-modify-playback-state",
+	"user-read-currently-playing",
+	"app-remote-control",
+	"streaming",
+	"playlist-modify-public",
+	"playlist-modify-private",
+	"playlist-read-private",
+	"playlist-read-collaborative",
+	"user-follow-modify",
+	"user-follow-read",
+	"user-library-modify",
+	"user-library-read",
+	"user-read-email",
+	"user-read-private" )
+
+	$scopeString = "&scope=$scope"
+
+	$authorizeURL = $authorizeURL + $clientString + $responseString + $redirect_uri + $scopeString
+	
+	# https://marckean.com/2015/09/21/use-powershell-to-make-rest-api-calls-using-json-oauth/
+	$ie = New-Object –comObject InternetExplorer.Application
+	$ie.visible = $true
+	$ie.navigate($authorizeURL)
+	do{ Start-Sleep 1 } until ( $ie.LocationURL -match 'code=([^&]*)' )
+	$code = $matches[1]
+	$ie.Quit()
+	return $code
 }
 
-function Authorization {
-$authorizeURL = "https://accounts.spotify.com/authorize?"
-	$redirectURL = "https://hxrsmurf.info"
-	$scope = "user-modify-playback-state user-read-currently-playing user-read-playback-state"
-	$clientId = $null
-	$authorizeURL = $authorizeURL + "client_id=$clientid" + "&redirect_uri=$redirectURL" + "&scope=$scope" + "&response_type=token"
-	$authorizeURL
-}
-
-function Player {
-
-	$token = $args[0]
-	$volume = $args[1]
-	$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-	$headers.Add("Authorization", "Bearer $token")
-	$playerURI = "https://api.spotify.com/v1/me/player"
-	$uri = $playerURI + "/devices"
-	$devices = Invoke-RestMethod -URI $uri -Headers $headers -Method GET
-	$deviceActive = $devices.devices.is_active
-	$deviceName = $devices.devices.name
-
-	if ($deviceActive -eq $true) {
-		echo "Checking playback on $deviceName"
-		$uri = $playerURI
-		$playback = Invoke-RestMethod -URI $uri -Headers $headers -Method GET		
-		$track = $playback.item.name	
-		$artist = $playback.item.artists.name
-		
-		if ($playback.item.artists.length -ne 1){
-			$artist = $artist[0]
-		}
-		
-		$nowPlaying =  "'$track' by '$artist'"
-		
-		if ($playback.is_playing -eq $false){
-			echo "Start playing $nowPlaying on $deviceName"
-			$uri = $playerURI + "/play"
-			Invoke-RestMethod -URI $uri -Headers $headers -Method PUT			
-		}  else {	
-			echo "Pausing $nowPlaying on $deviceName"
-			$uri = $playerURI + "/pause"
-			Invoke-RestMethod -URI $uri -Headers $headers -Method PUT	
-		}
-		
-		if ($playback.repeat_state -ne "context"){	
-			echo "Turning on repeat."
-			$uri = $playerURI + "/repeat"
-			$uri = $uri + "?state=context"
-			Invoke-RestMethod -URI $uri -Headers $headers -Method PUT	
-		}
-		
-		if ($devices.devices[0].volume_percent -ne $volume){		
-			$uri = $playerURI + "/volume"
-			$uri = $uri + "?volume_percent=$volume"		
-			Invoke-RestMethod -URI $uri -Headers $headers -Method PUT
-		}		
-	} else {
-		echo "No devices found."
-		return
+function getRefreshToken {
+	$client_id = ""
+	$client_secret = ""
+	$base64String = $client_id + ":" + $client_secret
+	$base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($base64String))
+	
+	$redirect_uri = "https://kvchmurphy.com/callback/"
+	$refresh_url = "https://accounts.spotify.com/api/token"
+	
+	$headers = @{
+		"Authorization" = "Basic $base64"
 	}
+	
+	$code = initialScope
+	
+	$data = @{
+		grant_type = "authorization_code"
+		code = $code
+		redirect_uri = "$redirect_uri"
+	}
+
+	$request = Invoke-WebRequest -URI $refresh_url -Method "POST" -Headers $headers -body $data
+	$request = $request.content | ConvertFrom-JSON
+	
+	$output = @{
+		access_token = $null
+		refresh_token = $null
+		expires_in = $null
+	}
+
+	$output.access_token = $request.access_token
+	$output.refresh_token = $request.refresh_token
+	$output.expires_in = $request.expires_in
+	
+	return $output
+}
+
+function refreshToken ($refresh_token) {
+	$data = @{
+			grant_type = "refresh_token"
+			refresh_token = $refresh_token
+	}
+	
+	$refresh_url = "https://accounts.spotify.com/api/token"
+	$request = Invoke-WebRequest -URI $refresh_url -Method "POST" -Headers $headers -body $data
+	$request = $request.content | ConvertFrom-JSON
+	
+	$access_token = $request.access_token
+	
+	return $access_token
+}
+
+function getUser ($access_token) {
+	$user_url = "https://api.spotify.com/v1/me"
+
+	$headers = @{
+		"Authorization" = "Bearer $access_token"
+	}
+
+	$request = Invoke-WebRequest -URI $user_url -Headers $headers
+	$request = $request.content | ConvertFrom-JSON
+	return $request
+}
+
+function getTop ($access_token){
+	$base_top_url = "https://api.spotify.com/v1/me/top/"
+	
+	$headers = @{
+		"Authorization" = "Bearer $access_token"
+	}
+
+	# artists or tracks
+	$type = "tracks"
+	
+	<#
+		time_range
+		long_term = several years
+		medium_term = 6-months
+		short_term = 4-weeks
+	#>
+	
+	$time_range = "short_term"
+	$limit = 50
+	
+	$top_url = $base_top_url + $type + "?time_range=$time_range&limit=$limit"
+	
+	$request = Invoke-WebRequest -URI $top_url -headers $headers
+	$request = $request.content | ConvertFrom-JSON
+	$items = $request.items
+	
+	foreach ($item in $items){
+		if ($type = "tracks"){
+			 Write-Host $item.name "-" $item.artists.name
+		} else {
+			Write-Host $item.name
+		}
+	}
+}
+
+function modifyPlayback ($access_token) {
+	$headers = @{
+		"Authorization" = "Bearer $access_token"
+	}
+	
+	$player_url = "https://api.spotify.com/v1/me/player"
+	$request = Invoke-WebRequest -URI $player_url -headers $headers
+	$request = $request.content | ConvertFrom-JSON
+	
+	if ($request.is_playing -eq $true){
+		$playback_url = "https://api.spotify.com/v1/me/player/pause"
+	} else {
+		$playback_url = "https://api.spotify.com/v1/me/player/play"
+	}
+	
+	$request = Invoke-WebRequest -URI $playback_url -Method "PUT" -Headers $headers
+}
+
+function getPlayer ($access_token){
+	$headers = @{
+		"Authorization" = "Bearer $access_token"
+	}
+	
+	$player_url = "https://api.spotify.com/v1/me/player"
+	$request = Invoke-WebRequest -URI $player_url -Method "GET" -Headers $headers
+	$request = $request.content | ConvertFrom-JSON
+	return $request
+	
+}
+
+function getPlaylists ($access_token){
+	$headers = @{
+		"Authorization" = "Bearer $access_token"
+	}
+	
+	$playlists_url = "https://api.spotify.com/v1/me/playlists" + "?limit=50"
+	$request = Invoke-WebRequest -URI $playlists_url -Method "GET" -Headers $headers
+	$request = $request.content | ConvertFrom-JSON
+	$items = $request.items
+	
+	foreach ($item in $items){
+		if ($item.name -like "Daily Mix*"){
+			Write-Host $item.name "|" $item.external_urls.spotify "|" $item.href
+		}
+	}
+}
+
+function login {
+	if ($refresh_token){
+		$access_token = refreshToken $refresh_token
+	} else {
+		if ($access_token -eq $null -or $refresh_token -eq $null){
+			$getTokens = getRefreshToken
+			$refresh_token = $getTokens.refresh_token
+			$access_token = $getTokens.access_token
+		}
+	}
+	
+	# Need to fix this, so only refresh token if needed.
+	
+	$refreshToken = getRefreshToken $refresh_token
+	$refresh_token = $refreshToken.refresh_token
+	$access_token = $refreshToken.access_token
+	
+	return $access_token
 }
