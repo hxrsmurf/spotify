574d5da3227b1ef681531fbb6cb5c1e9cdc83f0f
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-01 18:56:33 -0400

    Add play_state check

--- python/functions/dynamodb.py
+++ python/functions/dynamodb.py
@@ -2,16 +2,18 @@ import boto3
 from datetime import datetime
 import functions.ssm as ssm
 
-def put(now_playing, table, current_track_parameter):    
+def put(now_playing, table, current_track_parameter):
     # Set timestamp as 2022-01-10 18:59:00:00
     dt = datetime.now()
     timestamp = dt.strftime('%Y-%m-%d, %H:%M:%S:%f')
 
     recent_track = ssm.get(current_track_parameter)
-    
+
+    play_state = now_playing['playing']
+
     # This obviously doesn't track if a song is on repeat. But oh well.
-    if recent_track == now_playing['songID']:
-        print('Already in SSM/DynamoDB')
+    if not play_state:
+        print(f'Play state is {play_state}')
     else:
         client = boto3.client('dynamodb')
         response = client.update_item(
