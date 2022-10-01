2f1b1620e1f35dcd2e037656072f6ad583e9e129
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-01 09:30:46 -0400

    add try for printing

--- dynamodb-report/index.py
+++ dynamodb-report/index.py
@@ -16,12 +16,15 @@ def handler(event, context):
 
     print ('album', 'artist', 'song_id', 'device_id', 'device_type', 'device', 'id', 'album_id')
     for result in results:
-        album = result['album']['S']
-        artist = result['artist']['S']
-        song_id = result['songID']['S']
-        device_id = result['deviceID']['S']
-        device_type = result['deviceType']['S']
-        device = result['device']['S']
-        id = result['id']['S']
-        album_id = result['albumID']['S']
-        print (album, artist, song_id, device_id, device_type, device, id, album_id)
+        try:
+            album = result['album']['S']
+            artist = result['artist']['S']
+            song_id = result['songID']['S']
+            device_id = result['deviceID']['S']
+            device_type = result['deviceType']['S']
+            device = result['device']['S']
+            id = result['id']['S']
+            album_id = result['albumID']['S']
+            print (album, artist, song_id, device_id, device_type, device, id, album_id)
+        except:
+            print(result)
