01ab5eb3ea3fd57798864c19816790a89395972e
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-01 09:27:53 -0400

    Basic result print

--- dynamodb-report/index.py
+++ dynamodb-report/index.py
@@ -8,6 +8,17 @@ def handler(event, context):
     client = boto3.client('dynamodb')
     response = client.scan(
         TableName=table,
-        Limit=10
+        Limit=1
     )
-    print(response)
+    results = response['Items']
+    print ('album', 'artist', 'song_id', 'device_id', 'device_type', 'device', 'id', 'album_id')
+    for result in results:
+        album = result['album']['S']
+        artist = result['artist']['S']
+        song_id = result['songID']['S']
+        device_id = result['deviceID']['S']
+        device_type = result['deviceType']['S']
+        device = result['device']['S']
+        id = result['id']['S']
+        album_id = result['albumID']['S']
+        print (album, artist, song_id, device_id, device_type, device, id, album_id)
