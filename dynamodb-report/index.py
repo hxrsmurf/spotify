08728687e2bc7ba495b6ce6cb9dc93a12d68f1a6
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-01 09:20:15 -0400

    Basic scan

--- dynamodb-report/index.py
+++ dynamodb-report/index.py
@@ -4,3 +4,10 @@ import os
 def handler(event, context):
     table = os.environ['DynamoDBTable']
     print(f'Accessing {table}')
+
+    client = boto3.client('dynamodb')
+    response = client.scan(
+        TableName=table,
+        Limit=10
+    )
+    print(response)
