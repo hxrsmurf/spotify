e8f10fc05a13b0e68a44ad1fe32340a854315c3b
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-01 09:47:02 -0400

    Add Basic Dynamo DB Scan (#3)
    * Update python3.7 to python3.9
    * Add basic lamda for reports
    * Add basic lamda for reports
    * Add dynamodb lambda variable
    * Basic scan
    * Basic result print
    * Basic LastEvaluatedKey
    * add try for printing
    * add list of tracks array

---
+++ dynamodb-report/index.py
@@ -0,0 +1,34 @@
+import boto3
+import os
+
+def handler(event, context):
+    list_of_tracks = []
+    table = os.environ['DynamoDBTable']
+    print(f'Accessing {table}')
+
+    client = boto3.client('dynamodb')
+    response = client.scan(
+        TableName=table
+    )
+    results = response['Items']
+    if 'LastEvaluatedKey' in response:
+        last_key = response['LastEvaluatedKey']
+        print(last_key)
+
+    print ('album', 'artist', 'song_id', 'device_id', 'device_type', 'device', 'id', 'album_id')
+    for result in results:
+        try:
+            album = result['album']['S']
+            artist = result['artist']['S']
+            song_id = result['songID']['S']
+            device_id = result['deviceID']['S']
+            device_type = result['deviceType']['S']
+            device = result['device']['S']
+            id = result['id']['S']
+            album_id = result['albumID']['S']
+            total_result = album, artist, song_id, device_id, device_type, device, id, album_id
+            list_of_tracks.append(total_result)
+            print(total_result)
+        except:
+            print(result)
+    #print(list_of_tracks)
