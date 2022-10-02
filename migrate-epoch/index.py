a56da5837e35a314e0d54a2f77d9168ac095904d
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-02 09:39:22 -0400

    Update dynamodb item in index

--- migrate-epoch/index.py
+++ migrate-epoch/index.py
@@ -2,6 +2,8 @@ import boto3
 import os
 from datetime import datetime
 
+from functions.dynamodb import put
+
 def handler(event, context):
     table = os.environ['DynamoDBTable']
     print(f'Accessing {table}')
@@ -11,29 +13,13 @@ def handler(event, context):
         TableName=table,
         Limit=1
     )
-    results = response['Items']
-    if 'LastEvaluatedKey' in response:
-        last_key = response['LastEvaluatedKey']
-        print(last_key)
 
-    print ('album', 'artist', 'song_id', 'device_id', 'device_type', 'device', 'id', 'album_id')
+    results = response['Items']
+
     for result in results:
-        if 'id' in result:
+        if not 'epochTime' in result:
             id = result['id']['S']
             datetime_object = datetime.strptime(id, '%Y-%m-%d, %H:%M:%S:%f')
             epoch_time = (datetime_object).timestamp()
 
-        try:
-            album = result['album']['S']
-            artist = result['artist']['S']
-            song_id = result['songID']['S']
-            device_id = result['deviceID']['S']
-            device_type = result['deviceType']['S']
-            device = result['device']['S']
-            id = result['id']['S']
-            album_id = result['albumID']['S']
-            total_result = album, artist, song_id, device_id, device_type, device, id, album_id
-            list_of_tracks.append(total_result)
-            print(total_result)
-        except:
-            print(result)
+            put(id, epoch_time)
