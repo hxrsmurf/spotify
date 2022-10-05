c8e5452da2a4851c11c51af7522907185e55346d
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 18:59:46 -0400

    Use float for utils.py

--- python/functions/utils.py
+++ python/functions/utils.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 
-def calculate_duration_between_last_entry(previous, current):
-    duration = current - previous
+def calculate_duration_since_last_entry(previous, current):
+    duration = float(current) - float(previous)
     minutes = datetime.utcfromtimestamp(duration).strftime('%M')
     seconds = datetime.utcfromtimestamp(duration).strftime('%S')
     seconds_milliseconds = convert_to_milliseconds(seconds, 'seconds')
     minutes_milliseconds = convert_to_milliseconds(minutes, 'minutes')
-    return int(seconds_milliseconds + minutes_milliseconds)
+    return float(seconds_milliseconds + minutes_milliseconds)
 
 def convert_to_milliseconds(string, type):
     if type == 'seconds':
-        return int(string) * 1000
+        return float(string) * 1000
     elif type == 'minutes':
-        return int(string) * 60000
+        return float(string) * 60000
