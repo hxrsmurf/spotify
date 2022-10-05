3fdde5fdb4ad7580a64d80c319b3997599c053c9
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 06:00:28 -0400

    Remove scan in index

--- dynamodb-delete-by-id/index.py
+++ dynamodb-delete-by-id/index.py
@@ -6,7 +6,6 @@ from functions.dynamodb import db_delete
 
 def handler(event, context):
     table = os.environ['DynamoDBTable']
-    value = '2022-10-02, 00:05:58:782681'
 
     with open('ids.txt', 'r') as ids_to_delete:
         for id in ids_to_delete:
@@ -14,16 +13,3 @@ def handler(event, context):
             db_delete(id)
 
     print(f'Accessing {table}')
-
-    client = boto3.client('dynamodb')
-    response = client.query(
-        TableName=table,
-        KeyConditionExpression='id = :value',
-        ExpressionAttributeValues={
-            ':value' : {
-                'S' : value
-            }
-        }
-    )
-
-    print(response)
