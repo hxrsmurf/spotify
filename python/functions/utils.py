90b51b12efbb9712bb844056915b8651822e080f
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 18:34:45 -0400

    Add calculate_duration_between_last_entry to utils.py

--- python/functions/utils.py
+++ python/functions/utils.py
@@ -1,5 +1,13 @@
 from datetime import datetime
 
+def calculate_duration_between_last_entry(previous, current):
+    duration = current - previous
+    minutes = datetime.utcfromtimestamp(duration).strftime('%M')
+    seconds = datetime.utcfromtimestamp(duration).strftime('%S')
+    seconds_milliseconds = convert_to_milliseconds(seconds, 'seconds')
+    minutes_milliseconds = convert_to_milliseconds(minutes, 'minutes')
+    return int(seconds_milliseconds + minutes_milliseconds)
+
 def convert_to_milliseconds(string, type):
     if type == 'seconds':
         return int(string) * 1000
