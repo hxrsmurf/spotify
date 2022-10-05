c4e34647fc074f26bfd998cce37f8b872d434de4
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 06:00:37 -0400

    Move print to top

--- dynamodb-delete-by-id/index.py
+++ dynamodb-delete-by-id/index.py
@@ -6,10 +6,9 @@ from functions.dynamodb import db_delete
 
 def handler(event, context):
     table = os.environ['DynamoDBTable']
+    print(f'Accessing {table}')
 
     with open('ids.txt', 'r') as ids_to_delete:
         for id in ids_to_delete:
             print(f'Deleting {id}...')
             db_delete(id)
-
-    print(f'Accessing {table}')
