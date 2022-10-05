4adec393b6237853a554a0bb1896fba62c09ebdc
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-04 21:06:55 -0400

    Add plotting with labels

--- streaming_history/parser.py
+++ streaming_history/parser.py
@@ -1,21 +1,34 @@
 import pandas as pd
+import matplotlib.pyplot as plot
 
 def artists(df):
-    group = df.groupby(['year', 'artist_name'])['file'].count().nlargest(n=20, keep='all')
+    group = df.groupby(['artist_name'])['file'].count().nlargest(n=10, keep='all')
     group.to_excel('artists.xlsx', index=True)
-    print(group)
+    return group
 
 def tracks(df):
-    group = df.groupby(['year', 'track_name'])['file'].count().nlargest(n=20, keep='all')
+    group = df.groupby(['track_name'])['file'].count().nlargest(n=10, keep='all')
     group.to_excel('tracks.xlsx', index=True)
-    print(group)
+    return group
 
 def table(df):
     table = pd.pivot_table(df,values='file', index=['year', 'artist_name'], aggfunc='count')
     print(table)
 
+def plotty(group, type):
+    f = plot.figure()
+    f.set_figwidth(20)
+    f.set_figheight(10)
+
+    group.plot.bar(x=type, y='file', title=f'Top 2022 {type}', rot=0, width=.5)
+
+    for i in range(10):
+        plot.text(x=i - .1, y=group[i] + 2, s=group[i], color='black', fontweight='bold')
+
+    plot.show()
+
 df = pd.read_csv(r'output7.csv', sep=';')
 df = df[df['year'] == 2022]
 
-artists(df)
-tracks(df)
+plotty(artists(df), 'artist_name')
+plotty(tracks(df), 'track_name')
