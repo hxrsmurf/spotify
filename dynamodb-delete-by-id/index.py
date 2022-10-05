3cce1b1fd521b30b057a2fffe0901fa03a4fa116
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 05:56:26 -0400

    Add with open statement

--- dynamodb-delete-by-id/index.py
+++ dynamodb-delete-by-id/index.py
@@ -8,6 +8,10 @@ def handler(event, context):
     table = os.environ['DynamoDBTable']
     value = '2022-10-02, 00:05:58:782681'
 
+    with open('ids.txt', 'r') as ids_to_delete:
+        for id in ids_to_delete:
+            print(id)
+
     print(f'Accessing {table}')
 
     client = boto3.client('dynamodb')
