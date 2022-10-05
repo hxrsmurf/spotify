24ac4dc41b8485b89c1153c505e8efd17cdcaf18
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 05:59:57 -0400

    Create db_delete function

--- dynamodb-delete-by-id/functions/dynamodb.py
+++ dynamodb-delete-by-id/functions/dynamodb.py
@@ -3,5 +3,13 @@ import os
 
 def db_delete(id):
     table = os.environ['DynamoDBTable']
+    client = boto3.client('dynamodb')
 
-    print(id)
+    response = client.delete_item(
+        TableName = table,
+        Key = {
+            'id' : {
+                'S' : id
+            }
+        }
+    )
