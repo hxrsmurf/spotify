f0397b63211c9e208131831d8eb3ad9d58a92187
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-05 19:57:05 -0400

    Add strip() to db_delete (#23)

--- dynamodb-delete-by-id/index.py
+++ dynamodb-delete-by-id/index.py
@@ -11,4 +11,4 @@ def handler(event, context):
     with open('ids.txt', 'r') as ids_to_delete:
         for id in ids_to_delete:
             print(f'Deleting {id}...')
-            db_delete(id)
+            db_delete(id.strip())
