f62d7ffb2e998bf651cd97d12412cfe61c2c81b6
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-06 19:45:31 -0400

    Basic KeyConditions query

--- dynamodb-query/index.py
+++ dynamodb-query/index.py
@@ -1,14 +1,24 @@
 import boto3
 import os
 
-def index(event, context):
+def handler(event, context):
     client = boto3.client('dynamodb')
     table = os.environ['DynamoDBTable']
     global_secondary_index = 'year_month-index'
 
     response = client.query(
         TableName = table,
-        IndexName = global_secondary_index
+        IndexName = global_secondary_index,
+        KeyConditions={
+            'year_month' : {
+                'AttributeValueList' : [
+                    {
+                        'S' : '2022-10'
+                    }
+                ],
+                'ComparisonOperator' : 'EQ'
+            }
+        }
     )
 
     for results in response:
