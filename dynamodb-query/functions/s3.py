a4dfc4ac8f0119c9c20f454c835a5560c6ce793c
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-06 20:55:16 -0400

    Add current_day to s3.py

--- dynamodb-query/functions/s3.py
+++ dynamodb-query/functions/s3.py
@@ -1,7 +1,9 @@
 import boto3
 import os
 
+from .utils import current_day
+
 def s3_upload(file):
     bucket = os.environ['Bucket']
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(file, bucket, 'destination.csv')
+    s3.meta.client.upload_file(file, bucket, f'{current_day}.csv')
