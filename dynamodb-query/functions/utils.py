112c82c7dd278c7ed57d9fc7b146ac744f2792de
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-07 08:14:44 -0400

    Feature - Add Try/Except for dynamodb_query (#35)
    * Add try/except for variables
    * Move None sets to top
    * Move id and change list_of_results

--- dynamodb-query/functions/utils.py
+++ dynamodb-query/functions/utils.py
@@ -9,11 +9,26 @@ def current_day():
 def parse_db(results_db_query):
     list_of_results = []
     for result in results_db_query:
-        id = result['id']['S']
-        context_type = None
-        context_uri = None
-        track_duration_ms = None
-        possible_duplicate = None
+        album =  None
+        artist =  None
+        song =  None
+        songID =  None
+        deviceID =  None
+        epochTime =  None
+        deviceType =  None
+        device =  None
+        year_month =  None
+        id =  None
+        albumID =  None
+        contextType =  None
+        contextUri =  None
+        trackDurationMS =  None
+        possibleDuplicate =  None
+        
+        try:
+            id = result['id']['S']
+        except:
+            pass
 
         # Some entries in the DB may not have these new values
         try:
@@ -21,26 +36,36 @@ def parse_db(results_db_query):
             context_uri = result['contextUri']['S']
             track_duration_ms = result['trackDurationMS']['N']
             possible_duplicate = result['possibleDuplicate']['BOOL']
+            epochTime = result['epochTime']['N']
+            album = result['album']['S']
+            artist = result['artist']['S']
+            song =  result['song']['S']
+            songID = result['songID']['S']
+            deviceID = result['deviceID']['S']
+            deviceType = result['deviceType']['S']
+            device = result['device']['S']
+            year_month = result['year_month']['S']
+            albumID = result['albumID']['S']
         except Exception as e:
             print(f'{id} empty due to error: {e}')
 
         list_of_results.append(
             {
-                "album": result['album']['S'],
-                "artist": result['artist']['S'],
-                "song": result['song']['S'],
-                "songID": result['songID']['S'],
-                "deviceID": result['deviceID']['S'],
-                "epochTime": result['epochTime']['N'],
-                "deviceType":result['deviceType']['S'],
-                "device": result['device']['S'],
-                "year_month": result['year_month']['S'],
-                "id": result['id']['S'],
-                "albumID": result['albumID']['S'],
-                "contextType" : context_type,
-                "contextUri" : context_uri,
-                "trackDurationMS" : track_duration_ms,
-                "possibleDuplicate" : possible_duplicate
+                "album": album,
+                "artist": artist,
+                "song": song,
+                "songID": songID,
+                "deviceID": deviceID,
+                "epochTime": epochTime,
+                "deviceType": deviceType,
+                "device": device,
+                "year_month": year_month,
+                "id": id,
+                "albumID": albumID,
+                "contextType" : contextType,
+                "contextUri" : contextUri,
+                "trackDurationMS" : trackDurationMS,
+                "possibleDuplicate" : possibleDuplicate
             }
         )
     return list_of_results
