1cb13ee81e53e8cd9a6305ca8a6abc8330bc8f93
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-07 08:13:47 -0400

    Move idand change list_of_results

--- dynamodb-query/functions/utils.py
+++ dynamodb-query/functions/utils.py
@@ -9,7 +9,6 @@ def current_day():
 def parse_db(results_db_query):
     list_of_results = []
     for result in results_db_query:
-        id = result['id']['S']
         album =  None
         artist =  None
         song =  None
@@ -25,6 +24,11 @@ def parse_db(results_db_query):
         contextUri =  None
         trackDurationMS =  None
         possibleDuplicate =  None
+        
+        try:
+            id = result['id']['S']
+        except:
+            pass
 
         # Some entries in the DB may not have these new values
         try:
@@ -41,7 +45,6 @@ def parse_db(results_db_query):
             deviceType = result['deviceType']['S']
             device = result['device']['S']
             year_month = result['year_month']['S']
-            id = result['id']['S']
             albumID = result['albumID']['S']
         except Exception as e:
             print(f'{id} empty due to error: {e}')
@@ -59,10 +62,10 @@ def parse_db(results_db_query):
                 "year_month": year_month,
                 "id": id,
                 "albumID": albumID,
-                "contextType" : context_type,
-                "contextUri" : context_uri,
-                "trackDurationMS" : track_duration_ms,
-                "possibleDuplicate" : possible_duplicate
+                "contextType" : contextType,
+                "contextUri" : contextUri,
+                "trackDurationMS" : trackDurationMS,
+                "possibleDuplicate" : possibleDuplicate
             }
         )
     return list_of_results
