e64bb0aba9e648834c3a5b29f11788c66a0427d9
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-06 20:16:40 -0400

    Add pandas import to index.py

--- dynamodb-query/index.py
+++ dynamodb-query/index.py
@@ -3,8 +3,10 @@ import os
 
 from functions.dynamodb import db_query
 from functions.utils import parse_db
+import pandas as pd
 
 def handler(event, context):
     results_db_query = db_query()
     results_parse_db = parse_db(results_db_query)
-    return(results_parse_db)
+    print(pd.DataFrame(results_parse_db))
+    return('kevin')
