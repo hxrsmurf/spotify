79e6d0f6aab9ab0fb458a7db54c1be157123ae22
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-06 20:55:30 -0400

    Add try/except for new table columns

--- dynamodb-query/functions/utils.py
+++ dynamodb-query/functions/utils.py
@@ -9,6 +9,21 @@ def current_day():
 def parse_db(results_db_query):
     list_of_results = []
     for result in results_db_query:
+        id = result['id']['S']
+        context_type = None
+        context_uri = None
+        track_duration_ms = None
+        possible_duplicate = None
+
+        # Some entries in the DB may not have these new values
+        try:
+            context_type = result['contextType']['S']
+            context_uri = result['contextUri']['S']
+            track_duration_ms = result['trackDurationMS']['N']
+            possible_duplicate = result['possibleDuplicate']['BOOL']
+        except Exception as e:
+            print(f'{id} empty due to error: {e}')
+
         list_of_results.append(
             {
                 "album": result['album']['S'],
@@ -21,7 +36,11 @@ def parse_db(results_db_query):
                 "device": result['device']['S'],
                 "year_month": result['year_month']['S'],
                 "id": result['id']['S'],
-                "albumID": result['albumID']['S']
+                "albumID": result['albumID']['S'],
+                "contextType" : context_type,
+                "contextUri" : context_uri,
+                "trackDurationMS" : track_duration_ms,
+                "possibleDuplicate" : possible_duplicate
             }
         )
     return list_of_results
