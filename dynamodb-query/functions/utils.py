724627bc4abfacad93f802d03458f15cfcf1e0bd
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-06 20:14:46 -0400

    Add parse_db utility function

--- dynamodb-query/functions/utils.py
+++ dynamodb-query/functions/utils.py
@@ -2,3 +2,24 @@ from datetime import datetime
 
 def current_year_month():
     return datetime.utcnow().strftime('%Y-%m')
+
+def parse_db(results_db_query):
+    list_of_results = []
+    for result in results_db_query:
+        print(result)
+        list_of_results.append(
+            {
+                "album": result['album']['S'],
+                "artist": result['artist']['S'],
+                "song": result['song']['S'],
+                "songID": result['songID']['S'],
+                "deviceID": result['deviceID']['S'],
+                "epochTime": result['epochTime']['N'],
+                "deviceType":result['deviceType']['S'],
+                "device": result['device']['S'],
+                "year_month": result['year_month']['S'],
+                "id": result['id']['S'],
+                "albumID": result['albumID']['S']
+            }
+        )
+    return list_of_results
--- dynamodb-query/index.py
+++ dynamodb-query/index.py
@@ -2,7 +2,9 @@ import boto3
 import os
 
 from functions.dynamodb import db_query
+from functions.utils import parse_db
 
 def handler(event, context):
     results_db_query = db_query()
-    return(results_db_query)
+    results_parse_db = parse_db(results_db_query)
+    return(results_parse_db)
