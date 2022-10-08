051010bbd0d223245865fa9c3d93c3cd3b093a58
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-08 10:25:16 -0400

    Add code

---
+++ 3_Utilities/migrations/add-year-month/code/functions/dynamodb.py
@@ -0,0 +1,24 @@
+import boto3
+import os
+
+def db_put(id, year_month):
+    table = os.environ['Table']
+    client = boto3.client('dynamodb')
+
+    print(f'Updating {id} with {year_month}')
+
+    return client.update_item(
+        TableName=table,
+        Key={
+            'id': {
+                'S': id
+            }
+        },
+        AttributeUpdates={
+                'year_month': {
+                        'Value': {
+                            'S': str(year_month)
+                        }
+                }
+        }
+    )
