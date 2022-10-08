beb2fa54e611ae42b9c10e1f8c21dc340989b3b5
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-07 23:29:17 -0400

    Delete the functions

--- python/functions/eventbridge.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-def update(value):
-    client = boto3.client('events')
-    schedule_expression = f'rate({value} minutes)'
-    response = client.put_rule(
-        Name='EventBridge',
-        ScheduleExpression=schedule_expression
-    )
