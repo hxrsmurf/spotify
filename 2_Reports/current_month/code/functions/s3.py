163213fc8b4006e1a35f98c4eccb575c8ac6e5b3
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-08 19:05:32 -0400

    Upload to outputs folder

--- 2_Reports/current_month/code/functions/s3.py
+++ 2_Reports/current_month/code/functions/s3.py
@@ -6,4 +6,4 @@ from .utils import current_day_time
 def s3_upload(file):
     bucket = os.environ['Bucket']
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(file, bucket, f'{current_day_time()}.csv')
+    s3.meta.client.upload_file(file, bucket, f'outputs/{current_day_time()}.csv')
