a9736314bf7c0a2ab1d70281684e526f790867c5
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-08 09:58:22 -0400

    Add current_month report code

---
+++ 2_Reports/current_month/code/functions/dynamodb.py
@@ -0,0 +1,29 @@
+import boto3
+import os
+
+from .utils import current_year_month
+
+def db_query():
+    client = boto3.client('dynamodb')
+    table = os.environ['Table']
+    global_secondary_index = 'year_month-id-index'
+
+    response = client.query(
+        TableName = table,
+        IndexName = global_secondary_index,
+        #Limit = 10,
+        KeyConditionExpression = 'year_month = :value',
+        ExpressionAttributeValues = {
+            ':value' : {
+                'S' : current_year_month()
+            }
+        }
+    )
+
+    items_counted = response['Count']
+    items_scanned = response['ScannedCount']
+
+    print(f'Count: {items_counted}')
+    print(f'Scanned: {items_scanned}')
+
+    return response['Items']
---
+++ 2_Reports/current_month/code/functions/s3.py
@@ -0,0 +1,9 @@
+import boto3
+import os
+
+from .utils import current_day_time
+
+def s3_upload(file):
+    bucket = os.environ['Bucket']
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(file, bucket, f'{current_day_time()}.csv')
