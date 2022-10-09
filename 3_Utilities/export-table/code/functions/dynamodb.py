5069255b110d485dc1bb6bc225a670a6690fa3be
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-08 20:25:31 -0400

    Update to TableArn

--- 3_Utilities/export-table/code/functions/dynamodb.py
+++ 3_Utilities/export-table/code/functions/dynamodb.py
@@ -4,13 +4,13 @@ import os
 from .utils import current_day_time
 
 def db_export():
-    table = os.environ['Table']
+    table_arn = os.environ['TableArn']
     bucket = os.environ['Bucket']
     client = boto3.client('dynamodb')
     outut_file = f'{current_day_time}.csv'
 
     response = client.export_table_to_point_in_time(
-        TableName = table,
+        TableArn  = table_arn,
         S3Bucket = bucket,
         S3Prefix = f'output/{outut_file}'
     )
