1d6d65076d68823e84ef2e85580b10de2b2e8798
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-09 12:55:08 -0400

    Update current_day_time() [skip ci]

--- 3_Utilities/export-table/code/functions/dynamodb.py
+++ 3_Utilities/export-table/code/functions/dynamodb.py
@@ -9,12 +9,12 @@ def db_export():
     table_arn = os.environ['TableArn']
     bucket = os.environ['Bucket']
     client = boto3.client('dynamodb')
-    outut_file = f'{current_day_time}.csv'
+    output_file = f'{current_day_time()}.csv'
 
     response = client.export_table_to_point_in_time(
         TableArn  = table_arn,
         S3Bucket = bucket,
-        S3Prefix = f'output/{outut_file}'
+        S3Prefix = f'output/{output_file}'
     )
 
     return response['ExportDescription']['ExportArn']
