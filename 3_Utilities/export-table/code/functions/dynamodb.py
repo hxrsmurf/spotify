1d27b0d2aa7eb72f0d50221649d6a30f69dfebd0
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-08 20:06:27 -0400

    Add code

---
+++ 3_Utilities/export-table/code/functions/dynamodb.py
@@ -0,0 +1,16 @@
+import boto3
+import os
+
+from .utils import current_day_time
+
+def db_export(id):
+    table = os.environ['Table']
+    bucket = os.environ['Bucket']
+    client = boto3.client('dynamodb')
+    outut_file = f'{current_day_time}.csv'
+
+    response = client.export_table_to_point_in_time(
+        TableName = table,
+        S3Bucket = bucket,
+        S3Prefix = f'output/{outut_file}'
+    )
