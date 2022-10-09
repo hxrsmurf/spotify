ea4c596055d7dec6423396fa678514f4f2361cd6
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-09 12:47:45 -0400

    Add send_notfication

--- 3_Utilities/export-table/code/index.py
+++ 3_Utilities/export-table/code/index.py
@@ -2,12 +2,13 @@ import os
 
 from functions.dynamodb import db_export, db_describe_export
 from functions.ssm import ssm_put
+from functions.sns import send_notfication
 
 def handler(event, context):
     table = os.environ['TableArn']
     print(f'Exporting {table}')
 
     result_export = db_export()
-    print(result_export['ExportArn'])
-
-    status_export = db_describe_export()
+    message = f'Started DB Export: {result_export['ExportArn']}'
+    print(message)
+    send_notfication(message)
