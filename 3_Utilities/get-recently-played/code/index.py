de12515e1430d38da1617bf966dade3afff8f714
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-10 20:16:47 -0400

    update index.py

--- 3_Utilities/get-recently-played/code/index.py
+++ 3_Utilities/get-recently-played/code/index.py
@@ -1,4 +1,6 @@
-import boto3
+from functions.player import get_recently_played_tracks
+from functions.utils import handle_recently_played_tracks
 
 def handler(event, context):
-    print('Hello World')
+    recently_played_tracks = handle_recently_played_tracks(get_recently_played_tracks())
+    return recently_played_tracks
