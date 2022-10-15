c856ba62dec129bd33a1501627b376c74f499245
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-15 12:26:38 -0400

    Add def parse_csv()

--- 3_Utilities/parse-spotify-export/index.py
+++ 3_Utilities/parse-spotify-export/index.py
@@ -23,3 +23,15 @@ def parse_json():
         if '.json' in file:
             print('Parsing file:', file)
             parse(file)
+
+def parse_csv():
+    file = f'{current_utc_hour()}.csv'
+    df = pd.read_csv(file)
+
+    df[['full_date', 'full_time']] = df.ts.str.split('T', expand=True)
+    df[['year', 'month', 'day']] = df.full_date.str.split('-', expand=True)
+    df [['hour', 'minute', 'seconds']] =  df.full_time.str.split(':', expand=True)
+
+    # Columns to skip
+    df = df.drop(['conn_country', 'ms_played', 'ip_addr_decrypted', 'user_agent_decrypted', 'reason_start', 'reason_end', 'skipped','offline', 'offline_timestamp', 'incognito_mode'], axis=1)
+    df.to_csv(f'parsed_{file}')
