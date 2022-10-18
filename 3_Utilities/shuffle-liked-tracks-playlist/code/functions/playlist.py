202ca56de23e27a4a5040ef9456cf1417e75fc6c
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-17 22:03:46 -0400

    Feature - Create Playlist from Liked Songs (Daily) (#150)

---
+++ 3_Utilities/shuffle-liked-tracks-playlist/code/functions/playlist.py
@@ -0,0 +1,52 @@
+import json
+import requests
+
+from .utils import current_year_month_day
+
+def create_playlist(access_token):
+    spotify_url = 'https://api.spotify.com/v1/me/playlists'
+    headers = {
+        'Authorization' : 'Bearer ' + str(access_token),
+        'Content-Type' : 'application/json'
+    }
+    params = {'limit' : 50}
+
+    playlist_name = f'Shuffled Liked - {current_year_month_day()}'
+
+    body = {
+        'name': playlist_name,
+        'description' : playlist_name
+    }
+
+    response = requests.post(spotify_url, headers=headers, json=body)
+
+    status_code = response.status_code
+    if not status_code == 201:
+        print(f'Player Status Code {status_code}')
+        print(f'Player Content: {response.content}')
+    else:
+        response_json = json.loads(response.content)
+        return response_json['id']
+
+def create_shuffled_playlist(access_token, tracks):
+    playlist_id = create_playlist(access_token)
+    spotify_url = f'https://api.spotify.com/v1/playlists/{playlist_id}/tracks'
+
+    headers = {
+        'Authorization' : 'Bearer ' + str(access_token),
+        'Content-Type' : 'application/json'
+    }
+
+    body = {
+        'uris': tracks
+    }
+
+    response = requests.post(spotify_url, headers=headers, json=body)
+
+    status_code = response.status_code
+    if not status_code == 201:
+        print(f'Player Status Code {status_code}')
+        print(f'Player Content: {response.content}')
+    else:
+        response_json = json.loads(response.content)
+        print(response_json)
