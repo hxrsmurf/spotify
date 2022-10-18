d89fcd17b69542df057bfb83289fb51be1837107
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-17 21:44:50 -0400

    Move shuffle_tracks to utils

--- 3_Utilities/get-saved-tracks/code/functions/utils.py
+++ 3_Utilities/get-saved-tracks/code/functions/utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import random
+
 def handle_saved_tracks(all_tracks):
     list_of_tracks = []
     for tracks in all_tracks:
@@ -21,6 +23,22 @@ def handle_saved_tracks(all_tracks):
             })
 
     return list_of_tracks
+
+def shuffle_tracks(tracks):
+    new_track_list = []
+    random.shuffle(tracks)
+
+    tracks_added = 0
+    maximum_tracks = 100
+
+    for track in tracks:
+        new_track_list.append(track['uri'])
+        tracks_added += 1
+
+        if tracks_added == 100:
+            break
+
+    return new_track_list
 
 def current_year_month_day():
     return datetime.utcnow().strftime('%Y-%m-%d')
