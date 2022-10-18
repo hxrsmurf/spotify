05030b39bbe7fcb9c3a444b9d24d434096b62014
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-17 21:24:10 -0400

    Add create_playlist to index

--- 3_Utilities/get-saved-tracks/code/index.py
+++ 3_Utilities/get-saved-tracks/code/index.py
@@ -1,10 +1,14 @@
 from functions.authorization import get_access_token
 from functions.player import get_saved_tracks
 from functions.utils import handle_saved_tracks
+from functions.playlist import create_playlist
+
 import random
 
 def handler(event, context):
-    access_token = get_access_token()
-    tracks = get_saved_tracks()
+    tracks = get_saved_tracks(get_access_token())
     result_saved_tracks = handle_saved_tracks(tracks)
     random.shuffle(result_saved_tracks)
+    #new_playlist_id = create_playlist(get_access_token())
+    for track in result_saved_tracks:
+        print(track)
