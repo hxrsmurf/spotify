b8de353799967a4a8c324584549aba5ada2c45a7
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-17 20:11:55 -0400

    Add code

---
+++ 3_Utilities/get-liked-playlist/code/functions/player.py
@@ -0,0 +1,22 @@
+from email import header
+import json
+import requests
+from .authorization import get_access_token
+
+def get_playlists():
+    spotify_url = 'https://api.spotify.com/v1/me/playlists'
+    headers = {
+        'Authorization' : 'Bearer ' + str(get_access_token()),
+        'Content-Type' : 'application/json'
+    }
+
+    response = requests.get(spotify_url, headers=headers)
+
+    status_code = response.status_code
+
+    if not status_code == 200:
+        print(f'Player Status Code {status_code}')
+        print(f'Player Content: {response.content}')
+    else:
+        response_json = json.loads(response.content)
+        return response_json
