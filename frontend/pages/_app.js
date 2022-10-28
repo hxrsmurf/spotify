47407733507590f720221f9eab6b0a5a668411fa
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-28 18:22:06 -0400

    Add navigation to _app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,8 +5,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import Navigation from '../Components/Navigation';
+import { Container } from '@mui/material';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Container>
+        <Navigation/>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  )
 }
 
 export default MyApp
