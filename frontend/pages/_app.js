11feb94ea79602d24e53b83a8d65b05cf7494530
Kevin Murphy <kevin@hxrsmurf.info>
2022-10-29 10:10:08 -0400

    Add SessionProvider to _app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,22 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import Navigation from '../Components/Navigation';
-import { Container } from '@mui/material';
+import Navigation from "../Components/Navigation";
+import { Container } from "@mui/material";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
-      <Container>
-        <Navigation/>
-        <Component {...pageProps} />
-      </Container>
+      <SessionProvider session={session}>
+        <Container>
+          <Navigation />
+          <Component {...pageProps} />
+        </Container>
+      </SessionProvider>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
