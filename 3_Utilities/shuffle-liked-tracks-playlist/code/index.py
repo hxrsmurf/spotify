ff399bf5bc18b487e29ec8aeceb447f37036e24a
Kevin Murphy <kevin@hxrsmurf.info>
2023-01-25 02:58:11 +0000

    Comment out delete/unfollow playlist due to 401

--- 3_Utilities/shuffle-liked-tracks-playlist/code/index.py
+++ 3_Utilities/shuffle-liked-tracks-playlist/code/index.py
@@ -3,7 +3,7 @@ import os
 from functions.authorization import get_access_token
 from functions.player import get_saved_tracks
 from functions.utils import handle_saved_tracks, shuffle_tracks
-from functions.playlist import create_playlist, create_shuffled_playlist, unfollow_playlist
+from functions.playlist import create_playlist, create_shuffled_playlist, unfollow_playlist, check_user_follows_playlist
 from functions.ssm import put_parameter
 
 def handler(event, context):
@@ -12,7 +12,9 @@ def handler(event, context):
     shuffled_tracks = shuffle_tracks(handle_saved_tracks(tracks))
     new_playlist_id = create_shuffled_playlist(access_token=access_token, tracks=shuffled_tracks)
 
+    # Receiving 401 right now. Need to investigate the right authorization scope.
     # Receive New Playlist Id and Delete Old Playlist
-    parameter_ShuffledLikedPlaylistId = os.environ['ShuffledLikedPlaylistId']
-    unfollow_playlist(access_token=access_token, playlist_id=parameter_ShuffledLikedPlaylistId)
-    put_parameter(parameter=parameter_ShuffledLikedPlaylistId, value=new_playlist_id)
+    # parameter_ShuffledLikedPlaylistId = os.environ['ShuffledLikedPlaylistId']
+    # unfollow_playlist(access_token=access_token, playlist_id=parameter_ShuffledLikedPlaylistId)
+    # check_user_follows_playlist(access_token=access_token, playlist_id='6dppz7Cci1eoDdBFHIPPRe')
+    # put_parameter(parameter=parameter_ShuffledLikedPlaylistId, value=new_playlist_id)
