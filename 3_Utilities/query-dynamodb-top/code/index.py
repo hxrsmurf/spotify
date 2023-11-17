effeb4371f6a35d24b5219317ff3ede94e3cb2bb
Kevin Murphy <kevin@hxrsmurf.info>
2023-11-17 15:11:59 +0000

    Add catch if no query parameters

--- 3_Utilities/query-dynamodb-top/code/index.py
+++ 3_Utilities/query-dynamodb-top/code/index.py
@@ -5,7 +5,14 @@ from functions.dynamodb import query
 from functions.utils import parse_items, parse_query_string_parameters, get_current_year_month, create_pandas_data_frame
 
 def handler(event, context):
-    year_month, query_Type = parse_query_string_parameters(event)
+    year_month = get_current_year_month()
+
+    # If no query parameters
+    try:
+        year_month, query_Type = parse_query_string_parameters(event)
+    except:
+        pass
+
     items = query(year_month)
 
     if len(items) != 0:
@@ -13,6 +20,7 @@ def handler(event, context):
         df = create_pandas_data_frame(parsed_items)
 
         return {
+            "year_month": year_month,
             "top": json.loads(df),
             "items": parsed_items
         }
