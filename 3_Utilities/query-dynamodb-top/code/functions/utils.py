85544956eb5846f36fa251b1d4711f7d7cf27ebc
Kevin Murphy <kevin@hxrsmurf.info>
2023-11-19 09:53:57 +0000

    sum songs pandas merge

--- 3_Utilities/query-dynamodb-top/code/functions/utils.py
+++ 3_Utilities/query-dynamodb-top/code/functions/utils.py
@@ -57,14 +57,21 @@ def parse_query_string_parameters(event):
     
     return year_month, query_type, limit
 
-def create_pandas_data_frame(items, query_type='song', limit=10):
+def create_pandas_data_frame(items, query_type='song', limit=10, year=None):
     df = pd.DataFrame(items)
     df.pop('id')
     df['count'] = df.groupby(query_type)[query_type].transform('size')
     df.sort_values(by='count', ascending=False, inplace=True)
 
     if query_type == 'song':
-        new_df = df[['artist', 'song', 'album', 'year_month', 'count']].drop_duplicates()
+        new_df = df[['artist', 'song', 'songID', 'album', 'count']].drop_duplicates()
+        if year:
+            temp_df = new_df.groupby(['song', 'artist', 'count'])['count'].sum().reset_index(name = "total")
+            temp_df.sort_values(by="total", ascending=False, inplace=True)
+            new_df = pd.merge(new_df, temp_df, how='left', left_on=['artist', 'song'], right_on = ['artist', 'song'])
+            new_df = new_df[['artist', 'song', 'total']].drop_duplicates()
+            new_df.rename(columns={'total': 'count'}, inplace=True)
+
 
     if query_type == 'artist':
         new_df = df[['artist', 'year_month', 'count']].drop_duplicates()
@@ -81,37 +88,6 @@ def create_pandas_data_frame(items, query_type='song', limit=10):
         new_df = df[['device', 'year_month', 'count']].drop_duplicates()
     
     print(query_type)
-    # with pd.option_context('display.min_rows', 20):
-    #     print(new_df)
-    return json.loads(new_df.head(limit).to_json(orient='records'))
-
-def create_year_pandas_data_frame(items, query_type='song', limit=10):
-    df = pd.DataFrame(items)
-    df.pop('id')
-    df['count'] = df.groupby(query_type)[query_type].transform('size')
-    df.sort_values(by='count', ascending=False, inplace=True)
-
-    if query_type == 'songID':
-        new_df = df[['artist', 'song', 'songID', 'album', 'count']].drop_duplicates()
-        new_df = new_df.groupby(['song']).first().reset_index()
-        new_df.pop('songID')
-        new_df = new_df.sort_values(by='count', ascending=False)
-
-    if query_type == 'artist':
-        new_df = df[['artist', 'count']].drop_duplicates()
-
-    if query_type == 'album':
-        new_df = df[['album', 'albumID', 'count']].drop_duplicates()
-        new_df = new_df.groupby(['album']).first().reset_index()
-        new_df = new_df.sort_values(by='count', ascending=False)
-
-    if query_type == 'playlist_name':
-        new_df = df[['playlist_name', 'count']].drop_duplicates()
-
-    if query_type == 'device':
-        new_df = df[['device', 'count']].drop_duplicates()
-    
-    print(query_type)
-    with pd.option_context('display.min_rows', 100):
+    with pd.option_context('display.min_rows', 10):
         print(new_df)
     return json.loads(new_df.head(limit).to_json(orient='records'))
